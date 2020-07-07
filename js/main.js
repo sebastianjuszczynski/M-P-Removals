20149b9eeef961876d3799b3695f03e0a24f7709
Sebastian Juszczynski <sebastianjuszczynski@yahoo.com>
2020-07-07 21:06:13 +0100

    Fixed issues with navigation

--- js/main.js
+++ js/main.js
@@ -1,12 +1,11 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const nav = document.querySelector(".navbar");
 
-    function addShadow() {
-        if (window.scrollY >= 300) {
-            nav.classList.add("shadow-bg")
-        } else {
-            nav.classList.remove("shadow-bg")
-        }
-    }
-    window.addEventListener("scroll", addShadow)
-})
+
+    $(document).ready(function () {
+        $(document).click(function (event) {
+            var clickover = $(event.target);
+            var _opened = $(".navbar-collapse").hasClass("show");
+            if (_opened === true && !clickover.hasClass("navbar-toggler")) {
+                $(".navbar-toggler").click();
+            }
+        });
+    });
